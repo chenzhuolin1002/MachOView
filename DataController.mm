4662526cc4e0416fe9b462ae09a9d62d956e9847
chenzhuolin <gzchenzhuolin@corp.netease.com>
2019-11-28 21:08:49 +0800

    fix crash

--- DataController.mm
+++ DataController.mm
@@ -163,7 +163,8 @@ NSString * const MVStatusTaskTerminated           = @"MVStatusTaskTerminated";
 //-----------------------------------------------------------------------------
 - (void)writeString:(NSString *)str toFile:(FILE *)pFile
 {
-  fwrite(CSTRING(str), [str length] + 1, 1, pFile);
+  NSString *strWrite = str ?: @"";
+  fwrite(CSTRING(strWrite), [strWrite length] + 1, 1, pFile);
 }
 
 //-----------------------------------------------------------------------------
